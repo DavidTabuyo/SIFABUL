728936ee1141ac3fcd458f68a3be6a7dc87512a1
DavidTabuyo <dtabum00@estudiantes.unileon.es>
2023-11-28 14:28:06 +0100

    interfaz becario y inicio sesion

--- src/becario.py
+++ src/becario.py
@@ -1,7 +1,22 @@
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import uic
+from controlador import *
+
 
 class InterfazBecario(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('ui/becario.ui', self)
+        self.BtnFichar.clicked.connect(self.BtnFichar_clicked)
+
+
+
+    def BtnFichar_clicked(self):
+        #el becario ficha entrada o salida
+        #login()
+        ...
+
+
+
+
+
--- src/inicio_sesion.py
+++ src/inicio_sesion.py
@@ -1,8 +1,9 @@
 
-from PyQt5.QtWidgets import QMainWindow, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QMessageBox, QLineEdit
 from PyQt5 import uic
 from becario import InterfazBecario
 from responsable import InterfazResponsable
+from controlador import *
 
 
 esCorrecto=1 #0:no/1:si
@@ -16,6 +17,8 @@ class InicioSesion(QMainWindow):
         self.boton1.clicked.connect(self.button1_clicked)
         self.BotonOk.clicked.connect(self.BotonOk_clicked)
         self.nueva_ventana = None
+        self.Password.setEchoMode(QLineEdit.Password)  # Configurar el modo de eco
+
 
 
 
@@ -29,23 +32,21 @@ class InicioSesion(QMainWindow):
 
     #boton aceptar
     def BotonOk_clicked(self):
-        #comprobamos si es correcto o no
-        if esCorrecto:
-            self.close()
-            #usuario correcto, vemos si es admin o user
-            if esBecario:
-                #mostramos la interfaz de becario
-                self.nueva_ventana = InterfazBecario()
-            else:
-                #mostramos la interfaz de responsable
-                self.nueva_ventana = InterfazResponsable()
-            self.nueva_ventana.show()
-            
-        else:
-            # Crear y mostrar un cuadro de mensaje de error
+        #comprobamos si es correcto o n
+        try:
+            controlador = login(self.UserName.text(), self.Password.text())
+        except LookupError as e:
             mensaje_error = QMessageBox()
             mensaje_error.setIcon(QMessageBox.Critical)
             mensaje_error.setWindowTitle('Error')
-            mensaje_error.setText('Los datos introducidos no son correctos.')
+            mensaje_error.setText(str(e))
             mensaje_error.setStandardButtons(QMessageBox.Ok)
             mensaje_error.exec_()
+        except ValueError as e:
+            mensaje_error = QMessageBox()
+            mensaje_error.setIcon(QMessageBox.Critical)
+            mensaje_error.setWindowTitle('Error')
+            mensaje_error.setText(str(e))
+            mensaje_error.setStandardButtons(QMessageBox.Ok)
+            mensaje_error.exec_()
+        
