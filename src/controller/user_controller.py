d36b161be4bfbc525d7858328ce3049dc624d4b9
DavidTabuyo <dtabum00@estudiantes.unileon.es>
2023-12-13 17:45:41 +0100

    error commit

--- src/controller/user_controller.py
+++ src/controller/user_controller.py
@@ -10,3 +10,6 @@ class UserController:
     def get_weeks(self, worker_id: str, n: int) -> list[Week]:
         weeks = UserDao.get_weeks(worker_id)
         return [[None] * n + weeks][-n::]
+    
+    def change_password(self, old_password:str,new_password:str):
+        ...
